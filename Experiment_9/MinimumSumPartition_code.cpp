d817dff3d5517ed6d41e03a58c2390674154c615
Vishesh369 <vishesh99.8@hotmail.com>
2026-04-23 11:56:09 +0530

    Update MinimumSumPartition_code.cpp

--- Experiment_9/MinimumSumPartition_code.cpp
+++ Experiment_9/MinimumSumPartition_code.cpp
@@ -1,30 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int minDifference(vector<int>& arr) {
+    int minDifference(vector<int>& arr) 
+{
         int n = arr.size();
-
         int totalSum = 0;
-        for(int i = 0; i < n; i++)
-            totalSum += arr[i];
-
-        vector<bool> dp(totalSum + 1, false);
+        for (int x : arr) totalSum += x;
+    
+        int target = totalSum / 2;
+        vector<bool> dp(target + 1, false);
         dp[0] = true;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = totalSum; j >= arr[i]; j--) {
-                dp[j] = dp[j] || dp[j - arr[i]];
-            }
-        }
-
-        for(int j = totalSum / 2; j >= 0; j--) {
-            if(dp[j]) {
-                return totalSum - 2 * j;
-            }
-        }
-
-        return 0;
-    }
-};
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                if (dp[j - num]) {
+                    dp[j] = true;
+                }} }
+        int s1 = 0;
+        for (int i = target; i >= 0; i--) {
+            if (dp[i]) {
+                s1 = i;
+                break;
+            }} return totalSum - 2 * s1;
+    }};
